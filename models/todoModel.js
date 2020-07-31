b3e102c2485663ee5b755e92a4358b358f33d3be
Musfirotus <musfirotus@gmail.com>
2020-07-31 10:32:53 +0700

    fix todoModel

--- models/todoModel.js
+++ models/todoModel.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const todoSchema = new Schema({
+    _id: Number,
     item: String,
-    cek: String
-})
+    cek: {
+        type: Boolean,
+        default: false
+    }
+}, {collection: 'todomodels'});
 
 module.exports = mongoose.model('todoModel', todoSchema);
